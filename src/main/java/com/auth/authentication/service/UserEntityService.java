ab55a9eef74af1decd16ec0d23e00c8e682920dc
Anirudh <amirudhshanmukha2399@gmail.com>
2025-05-17 18:03:15 +0530

    changes

--- src/main/java/com/auth/authentication/service/UserEntityService.java
+++ src/main/java/com/auth/authentication/service/UserEntityService.java
@@ -8,8 +8,10 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
 @RequiredArgsConstructor
+@Service
 public class UserEntityService implements UserDetailsService {
     private final UserDetailsRepository userDetailsRepository;
     private final GenericMapper mapper;
